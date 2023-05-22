44c8971f4b9d89dc83d5a982be37063391f400af
Viscomsoft <support@viscomsoft.com>
2023-05-22 08:27:21 +0800

    Add files via upload

---
+++ c#2010/PDFWriter/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axImageViewer1 = new AxSCRIBBLELib.AxImageViewer();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.txtdate = new System.Windows.Forms.TextBox();
+            this.txtbillto = new System.Windows.Forms.TextBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.txtcaption = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.cbofont = new System.Windows.Forms.ComboBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.axImageViewer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axImageViewer1
+            // 
+            this.axImageViewer1.Enabled = true;
+            this.axImageViewer1.Location = new System.Drawing.Point(775, 86);
+            this.axImageViewer1.Name = "axImageViewer1";
+            this.axImageViewer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axImageViewer1.OcxState")));
+            this.axImageViewer1.Size = new System.Drawing.Size(282, 309);
+            this.axImageViewer1.TabIndex = 0;
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(37, 184);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(70, 24);
+            this.Label4.TabIndex = 18;
+            this.Label4.Text = "Date:";
+            // 
+            // txtdate
+            // 
+            this.txtdate.Location = new System.Drawing.Point(245, 184);
+            this.txtdate.Name = "txtdate";
+            this.txtdate.Size = new System.Drawing.Size(282, 35);
+            this.txtdate.TabIndex = 17;
+            this.txtdate.Text = "9-5-2023";
+            // 
+            // txtbillto
+            // 
+            this.txtbillto.Location = new System.Drawing.Point(245, 251);
+            this.txtbillto.Name = "txtbillto";
+            this.txtbillto.Size = new System.Drawing.Size(282, 35);
+            this.txtbillto.TabIndex = 16;
+            this.txtbillto.Text = "ABC Company";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(37, 254);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(106, 24);
+            this.Label3.TabIndex = 15;
+            this.Label3.Text = "Bill To:";
+            // 
+            // txtcaption
+            // 
+            this.txtcaption.Location = new System.Drawing.Point(245, 110);
+            this.txtcaption.Name = "txtcaption";
+            this.txtcaption.Size = new System.Drawing.Size(282, 35);
+            this.txtcaption.TabIndex = 14;
+            this.txtcaption.Text = "INVOICE";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(37, 110);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(94, 24);
+            this.Label2.TabIndex = 13;
+            this.Label2.Text = "Caption";
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(222, 357);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(242, 82);
+            this.Button1.TabIndex = 12;
+            this.Button1.Text = "Create PDF";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(37, 37);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(58, 24);
+            this.Label1.TabIndex = 11;
+            this.Label1.Text = "Font";
+            // 
+            // cbofont
+            // 
+            this.cbofont.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbofont.FormattingEnabled = true;
+            this.cbofont.Location = new System.Drawing.Point(245, 37);
+            this.cbofont.Name = "cbofont";
+            this.cbofont.Size = new System.Drawing.Size(282, 32);
+            this.cbofont.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1227, 486);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.txtdate);
+            this.Controls.Add(this.txtbillto);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.txtcaption);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.cbofont);
+            this.Controls.Add(this.axImageViewer1);
+            this.Name = "Form1";
+            this.Text = "PDF Writer Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axImageViewer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxSCRIBBLELib.AxImageViewer axImageViewer1;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.TextBox txtdate;
+        internal System.Windows.Forms.TextBox txtbillto;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox txtcaption;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.ComboBox cbofont;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ c#2010/PDFWriter/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbofont.Items.Add("Arial");
+            cbofont.Items.Add("Tahoma");
+            cbofont.Items.Add("Times New Roman");
+            cbofont.Items.Add("Courier New");
+            cbofont.SelectedIndex = 0;
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            string strFont;
+            bool bResult=false;
+            
+            strFont = cbofont.Text;
+
+        axImageViewer1.PDFWriterClearAll();
+        axImageViewer1.PDFWriterAddPage(4, false);
+
+        axImageViewer1.PDFWriterDrawImage(0, Application.StartupPath + "\\company-invoice-logo.jpg", 10, 100, 0.5, 0.5);
+        axImageViewer1.PDFWriterDrawText(0, strFont, true, false, false, 450, 50, txtcaption.Text, 30, 0, 0, 0);
+        axImageViewer1.PDFWriterDrawText(0, strFont, true, false, false, 450, 110, "Date:", 12, 0, 0, 0);
+
+    
+        //draw sample date
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 490, 110, txtdate.Text, 12, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, true, false, false, 450, 90, "Invoice No.:", 12, 0, 0, 0);
+
+       axImageViewer1.PDFWriterDrawText(0, strFont, true, false, false, 40, 150, "Bill To:", 12, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, true, false, false, 40, 200, "Name:", 12, 0, 0, 0);
+
+       axImageViewer1.PDFWriterDrawText(0, strFont, true, false,false, 40, 250, "Address:", 12, 0, 0, 0);
+
+       axImageViewer1.PDFWriterDrawText(0, strFont, true, false, false, 40, 300, "Email:", 12, 0, 0, 0);
+
+      
+        //draw sample company
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 96, 150, txtbillto.Text, 12, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawRectange(0, 40, 380, 550, 35, 1, 74, 74, 74, true);
+
+        //draw caption
+        axImageViewer1.PDFWriterDrawText(0, strFont, true, false, false, 40, 365, "DESCRIPTION", 18, 255, 255, 255);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, true, false, false, 250, 365, "UNIT", 18, 255, 255, 255);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, true, false, false, 350, 365, "UNIT PRICE", 18, 255, 255, 255);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, true, false, false, 500, 365, "AMOUNT", 18, 255, 255, 255);
+
+        //draw item 1
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false,false, 40, 400, "Item 1", 20, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 250, 400, "1", 20, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 350, 400, "100.00", 20, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, false,false, false, 500, 400, "$100.00", 20, 0, 0, 0);
+
+        //draw item 2
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 40, 430, "Item 2", 20, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 250, 430, "2", 20, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 350, 430, "100.00", 20, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 500, 430, "$200.00", 20, 0, 0, 0);
+
+        //draw item 3
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 40, 460, "Item 3", 20, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 250, 460, "2", 20, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 350, 460, "200.00", 20, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 500, 460, "$400.00", 20, 0, 0, 0);
+
+        //draw item 4
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 40, 490, "Item 4", 20, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 250, 490, "1", 20, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 350, 490, "500.00", 20, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 500, 490, "$500.00", 20, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawLine(0, 40, 498, 550 + 40, 495, 1, 0, 0, 0);
+        
+        //draw total
+        axImageViewer1.PDFWriterDrawText(0, strFont, true, false, false, 40, 540, "TOTAL", 18, 0, 0, 0);
+
+        axImageViewer1.PDFWriterDrawText(0, strFont, false, false, false, 496, 540, "$1200.00", 20, 0, 0, 0);
+
+         saveFileDialog1.Filter = "PDF (*.pdf)|*.pdf";
+
+
+        if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+        {
+
+            bResult = axImageViewer1.PDFWriterCreatePDF(saveFileDialog1.FileName, "my creator", "my author", "my title", "my subject", "my keywords", "my producer");
+
+            if (bResult)
+                MessageBox.Show("PDF Created");
+            
+
+        }
+
+
+        }
+    }
+}
