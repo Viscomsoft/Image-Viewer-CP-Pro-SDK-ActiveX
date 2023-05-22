b34b6fffb35564bd9948d45d69fcf224f39d48d0
Viscomsoft <support@viscomsoft.com>
2023-05-22 08:26:26 +0800

    Add files via upload

---
+++ VBdotNet2010/PDFWriter/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxImageViewer1 = New AxSCRIBBLELib.AxImageViewer()
+        Me.cbofont = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtcaption = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtbillto = New System.Windows.Forms.TextBox()
+        Me.txtdate = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        CType(Me.AxImageViewer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxImageViewer1
+        '
+        Me.AxImageViewer1.Enabled = True
+        Me.AxImageViewer1.Location = New System.Drawing.Point(824, 52)
+        Me.AxImageViewer1.Name = "AxImageViewer1"
+        Me.AxImageViewer1.OcxState = CType(resources.GetObject("AxImageViewer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxImageViewer1.Size = New System.Drawing.Size(138, 179)
+        Me.AxImageViewer1.TabIndex = 0
+        '
+        'cbofont
+        '
+        Me.cbofont.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbofont.FormattingEnabled = True
+        Me.cbofont.Location = New System.Drawing.Point(240, 36)
+        Me.cbofont.Name = "cbofont"
+        Me.cbofont.Size = New System.Drawing.Size(282, 32)
+        Me.cbofont.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 24)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Font"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(217, 356)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(242, 82)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Create PDF"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 109)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 24)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Caption"
+        '
+        'txtcaption
+        '
+        Me.txtcaption.Location = New System.Drawing.Point(240, 109)
+        Me.txtcaption.Name = "txtcaption"
+        Me.txtcaption.Size = New System.Drawing.Size(282, 35)
+        Me.txtcaption.TabIndex = 5
+        Me.txtcaption.Text = "INVOICE"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(32, 253)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(106, 24)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Bill To:"
+        '
+        'txtbillto
+        '
+        Me.txtbillto.Location = New System.Drawing.Point(240, 250)
+        Me.txtbillto.Name = "txtbillto"
+        Me.txtbillto.Size = New System.Drawing.Size(282, 35)
+        Me.txtbillto.TabIndex = 7
+        Me.txtbillto.Text = "ABC Company"
+        '
+        'txtdate
+        '
+        Me.txtdate.Location = New System.Drawing.Point(240, 183)
+        Me.txtdate.Name = "txtdate"
+        Me.txtdate.Size = New System.Drawing.Size(282, 35)
+        Me.txtdate.TabIndex = 8
+        Me.txtdate.Text = "9-5-2023"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(32, 183)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 24)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Date:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1288, 528)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtdate)
+        Me.Controls.Add(Me.txtbillto)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtcaption)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cbofont)
+        Me.Controls.Add(Me.AxImageViewer1)
+        Me.Name = "Form1"
+        Me.Text = "PDF Writer Demo"
+        CType(Me.AxImageViewer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents AxImageViewer1 As AxSCRIBBLELib.AxImageViewer
+    Friend WithEvents cbofont As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtcaption As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtbillto As System.Windows.Forms.TextBox
+    Friend WithEvents txtdate As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+
+End Class
---
+++ VBdotNet2010/PDFWriter/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,114 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        cbofont.Items.Add("Arial")
+        cbofont.Items.Add("Tahoma")
+        cbofont.Items.Add("Times New Roman")
+        cbofont.Items.Add("Courier New")
+        cbofont.SelectedIndex = 0
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim strFont As String
+        Dim bResult As Boolean
+        ' strFont = "Arial"
+        strFont = cbofont.Text
+
+        AxImageViewer1.PDFWriterClearAll()
+        AxImageViewer1.PDFWriterAddPage(4, False)
+
+        AxImageViewer1.PDFWriterDrawImage(0, Application.StartupPath + "\\company-invoice-logo.jpg", 10, 100, 0.5, 0.5)
+        AxImageViewer1.PDFWriterDrawText(0, strFont, True, False, False, 450, 50, txtcaption.Text, 30, 0, 0, 0)
+        AxImageViewer1.PDFWriterDrawText(0, strFont, True, False, False, 450, 110, "Date:", 12, 0, 0, 0)
+
+        'draw sample date
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 490, 110, txtdate.Text, 12, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, True, False, False, 450, 90, "Invoice No.:", 12, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, True, False, False, 40, 150, "Bill To:", 12, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, True, False, False, 40, 200, "Name:", 12, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, True, False, False, 40, 250, "Address:", 12, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, True, False, False, 40, 300, "Email:", 12, 0, 0, 0)
+
+        'draw sample company
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 96, 150, txtbillto.Text, 12, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawRectange(0, 40, 380, 550, 35, 1, 74, 74, 74, True)
+
+        'draw caption
+        AxImageViewer1.PDFWriterDrawText(0, strFont, True, False, False, 40, 365, "DESCRIPTION", 18, 255, 255, 255)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, True, False, False, 250, 365, "UNIT", 18, 255, 255, 255)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, True, False, False, 350, 365, "UNIT PRICE", 18, 255, 255, 255)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, True, False, False, 500, 365, "AMOUNT", 18, 255, 255, 255)
+
+        'draw item 1
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 40, 400, "Item 1", 20, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 250, 400, "1", 20, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 350, 400, "100.00", 20, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 500, 400, "$100.00", 20, 0, 0, 0)
+
+        'draw item 2
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 40, 430, "Item 2", 20, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 250, 430, "2", 20, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 350, 430, "100.00", 20, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 500, 430, "$200.00", 20, 0, 0, 0)
+
+        'draw item 3
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 40, 460, "Item 3", 20, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 250, 460, "2", 20, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 350, 460, "200.00", 20, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 500, 460, "$400.00", 20, 0, 0, 0)
+
+        'draw item 4
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 40, 490, "Item 4", 20, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 250, 490, "1", 20, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 350, 490, "500.00", 20, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 500, 490, "$500.00", 20, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawLine(0, 40, 498, 550 + 40, 495, 1, 0, 0, 0)
+
+        'draw total
+        AxImageViewer1.PDFWriterDrawText(0, strFont, True, False, False, 40, 540, "TOTAL", 18, 0, 0, 0)
+
+        AxImageViewer1.PDFWriterDrawText(0, strFont, False, False, False, 496, 540, "$1200.00", 20, 0, 0, 0)
+
+
+        Me.SaveFileDialog1.Filter = "PDF (*.pdf)|*.pdf"
+
+
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+
+
+            bResult = AxImageViewer1.PDFWriterCreatePDF(SaveFileDialog1.FileName, "my creator", "my author", "my title", "my subject", "my keywords", "my producer")
+
+            If bResult Then
+
+                MessageBox.Show("PDF Created")
+            End If
+
+        End If
+
+
+    End Sub
+End Class
